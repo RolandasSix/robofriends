1bbe25fd8b3435de15696ff7cfaee87a5a119ecb
Rol <rolis29@mail.com>
2022-04-24 17:15:40 +0300

    Updates

---
+++ precache-manifest.217e0ab57a72eb75e3363b27ef621708.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c620ec1fe447ca6c988",
+    "url": "/robofriends/static/css/main.faf70984.chunk.css"
+  },
+  {
+    "revision": "3c620ec1fe447ca6c988",
+    "url": "/robofriends/static/js/main.3c620ec1.chunk.js"
+  },
+  {
+    "revision": "99af9f38a1ea9e318f70",
+    "url": "/robofriends/static/css/1.dcd9f195.chunk.css"
+  },
+  {
+    "revision": "99af9f38a1ea9e318f70",
+    "url": "/robofriends/static/js/1.99af9f38.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "16cf5e05d1d47268744165020316a662",
+    "url": "/robofriends/index.html"
+  }
+];
